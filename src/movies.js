8c23f26f21e05f447b65cf1d937e93d9ffd46ccf
Smaranya <70739534+smaranya@users.noreply.github.com>
2022-08-03 21:58:07 +0530

    Add files via upload

---
+++ src/movies.js
@@ -0,0 +1,15 @@
+const movies = (movie) => {
+    return(
+        [
+            {
+                id: 1,
+                Poster: 'N/A',
+                Type: 'Comedy, Time Travel',
+                Title: 'Tokyo Revengers',
+                Year: '2020'
+            }
+        ]
+    )
+}
+
+export default movies;
